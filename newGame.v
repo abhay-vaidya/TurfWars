b24553eb412071139b75212a11238216c84a9319
Rahul Kumar Saini <rahul-kumar-saini@users.noreply.github.com>
2018-03-27 11:23:36 -0400

    Added ram updating, finished reading part of ram fsm

--- newGame.v
+++ newGame.v
@@ -8,7 +8,7 @@ module game(
 
   input CLOCK_50;
   output wire clonke;
-  output [4:0] KEY_PRESSED;
+  input [4:0] KEY_PRESSED;
 
   reg reset;
   initial begin
@@ -16,6 +16,10 @@ module game(
   end
 
   RateDivider div(CLOCK_50, clonke);
+
+  players p(); // initiate player regs
+  move m(clonke); // update player locations
+  directions d(CLOCK_50, clonke, KEY_PRESSED); // update player directions
 
   wire wren; // 1 : write data to the ram, 0 : don't write data to the ram
   wire [14:0] address; // 15 bits, 8 X bits, 7 Y bits
@@ -30,7 +34,14 @@ module game(
   	.q(out)
     );
 
-  move m(clonke);
+  ram_update update(
+    .CLOCK_50(CLOCK_50),
+    .clonke(),
+    .wren(wren),
+    .address(address),
+    .out(out),
+    .data(data)
+    );
 
 endmodule
 
@@ -54,15 +65,15 @@ endmodule
 
 module directions(
   CLOCK_50, clonke,
-  key_in
+  KEY_PRESSED
   );
 
   input CLOCK_50, clonke;
-  input [4:0] key_in;
+  input [4:0] KEY_PRESSED;
 
   always@(posedge CLOCK_50)
     begin
-      case (key_in)
+      case (KEY_PRESSED)
         5'd0: players.p1[16:15] <= 2'b00;
         5'd1: players.p1[16:15] <= 2'b01;
         5'd2: players.p1[16:15] <= 2'b10;
@@ -127,6 +138,110 @@ module move(clonke);
           2'b10: players.p4[14:7] <= players.p4[14:7] - 1'b1;
           2'b11: players.p4[14:7] <= players.p4[14:7] + 1'b1;
         endcase
+    end
+
+endmodule
+
+
+module ram_update(
+  CLOCK_50, clonke,
+  wren,
+  address,
+  out,
+  data
+  );
+
+  input CLOCK_50, clonke;
+
+  output reg wren;
+  output reg [14:0] address;
+  input [2:0] out;
+  output reg [2:0] data;
+
+  reg [5:0] ram_fsm, next_state;
+
+  localparam  sleep = 6'd0,
+              read_p1 = 6'd1,
+              read_p2 = 6'd2,
+              read_p3 = 6'd3,
+              read_p4 = 6'd4,
+              write_p1 = 6'd5,
+              write_p2 = 6'd6,
+              write_p3 = 6'd7,
+              write_p4 = 6'd8;
+
+  reg run, done;
+  assign run = !clonke && !done;
+
+  always@(*)
+    begin
+      case(ram_fsm)
+        sleep: next_state = run ? read_p1 : sleep;
+        read_p1: next_state = read_p2;
+        read_p2: next_state = read_p3;
+        read_p3: next_state = read_p4;
+        read_p4: next_state = write_p1;
+        write_p1: next_state = write_p2;
+        write_p2: next_state = write_p3;
+        write_p3: next_state = write_p4;
+        write_p4: next_state = sleep;
+        default: next_state = sleep;
+    end
+
+  always@(posedge clonke)
+    begin
+      done = 1'b0; // reset done every clonke cycle
+    end
+
+  reg [17:0] p1_curr, p2_curr, p3_curr, p4_curr; // [17:3] location, [2:0] colour
+
+  always@(*)
+    begin
+    wren = 1'b0;
+      case(ram_fsm)
+
+        read_p1: begin
+          address[14:0] <= players.p1[14:0];
+          p1_curr[17:3] <= players.p1[14:0]
+          p1_curr[2:0] <= out[2:0];
+        end
+
+        read_p2: begin
+          address[14:0] <= players.p2[14:0];
+          p2_curr[17:3] <= players.p2[14:0]
+          p2_curr[2:0] <= out[2:0];
+        end
+
+        read_p3: begin
+          address[14:0] <= players.p3[14:0];
+          p3_curr[17:3] <= players.p3[14:0]
+          p3_curr[2:0] <= out[2:0];
+        end
+
+        read_p4: begin
+          address[14:0] <= players.p4[14:0];
+          p4_curr[17:3] <= players.p4[14:0]
+          p4_curr[2:0] <= out[2:0];
+        end
+
+        write_p1: begin
+        end
+
+        write_p2: begin
+        end
+
+        write_p3: begin
+        end
+
+        write_p4: begin
+          done = 1'b1; // finished running
+        end
+      endcase
+    end
+
+  always@(posedge CLOCK_50)
+    begin
+      ram_fsm <= next_state;
     end
 
 endmodule
